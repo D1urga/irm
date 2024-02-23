8ca33fd628cb815c9bdb1c009f4a3a2b970b8446
unknown <64825962+D1urga@users.noreply.github.com>
2024-02-23 21:59:09 +0530

    added new file

--- src/app/components2/reportSection.js
+++ src/app/components2/reportSection.js
@@ -222,7 +222,7 @@ export default function ReportSection({
             ></input>
           </div>
           {assessmentLossFields.map((data, index) => (
-            <div>
+            <div key={index}>
               <p className={styles.des_title}>{data.name}</p>
               <textarea
                 value={assessmentLossTable[tablePage][`field${index}`]}
